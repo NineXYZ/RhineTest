6778f48f2c844c13079f1f8a632593d89ad897c1
NewCreative <ianparra200@gmail.com>
2020-06-01 21:55:44 -0400

    index created

---
+++ index.js
@@ -0,0 +1,8 @@
+// Creates the client
+const {Client} = require('discord.js');
+const client = new Client();
+
+// Start the client
+client.once('ready', () => console.log('Bot is ready!!!'));
+console.log('Reading messages...');
+client.on('message', msg => console.log(msg.content));
